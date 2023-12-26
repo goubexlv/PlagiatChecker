57f9c2466769e27c4a920703a39e046804cc4586
goubexlv <93274828+goubexlv@users.noreply.github.com>
2023-12-27 00:06:22 +0100

    es

--- PlagiatChecker/api/workfile.py
+++
@@ -1,7 +0,0 @@
-
-def Typefile(f):
-    ref_file=f
-    extension=ref_file.split('.')[-1]
-    return extension
-
-
--- PlagiatChecker/plagiatOnline/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def plagiatOnline(request):
-    return render(request, "online/index.html")
