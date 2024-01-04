e624cef04f76df6ba2ddee5d068b5119184394be
goubexlv <93274828+goubexlv@users.noreply.github.com>
2024-01-04 13:49:36 +0100

    Revert "Merge branch 'v0.1' into Gallagher_branch"
    This reverts commit 99564679253f38d94f6e2ec76a6ee69e2048f90c, reversing
    changes made to 315689c153f80c7baee74a7dca5af7e8b04fa354.

--- PlagiatChecker/PlagiatChecker/urls.py
+++ PlagiatChecker/PlagiatChecker/urls.py
@@ -6,8 +6,8 @@ from django.urls import path
 from .views import index
 from accounts.views import login_user,register_user,logout_user
 from plagiatDocuments.views import plagiatDocument
-from plagiatLocal.views import plagiatLocal, send_fichier , send_fichier2
-from plagiatOnline.views import plagiatOnline
+from plagiatLocal.views import plagiatLocal, send_fichier , send_fichier2 , plagiatLocalResponse, plagiatLocalResponse1
+from plagiatOnline.views import plagiatOnline,upload_file
 
 urlpatterns = [
     path("", login_user, name="login_user"),
@@ -22,11 +22,17 @@ urlpatterns = [
     
     # Dimitri et 45 
     path("plagiatLocal/", plagiatLocal ,name="plagiatLocal"),
+    path("plagiatLocals/", plagiatLocalResponse ,name="resultat"),
+    
     path("send/", send_fichier ,name="envoifichier"),
     path("send2/", send_fichier2 ,name="envoifichier2"),
     
     
     # Gallagher
-    path("plagiatOnline/", plagiatOnline ,name="plagiatOnline"),    
+    path("plagiatOnline/", plagiatOnline ,name="plagiatOnline"),   
+    path("upload_file/", upload_file ,name="upload_file"),    
+
+
+
 
 ]
