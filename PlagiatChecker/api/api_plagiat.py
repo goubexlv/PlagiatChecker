e5201f14ddfaa26e1f2bbe29e14dfe76a5f331e3
Franck Judes <93270724+FranckJudes@users.noreply.github.com>
2024-01-07 19:33:08 +0100

    07/01/2024  19:30

--- PlagiatChecker/api/api_plagiat.py
+++ PlagiatChecker/api/api_plagiat.py
@@ -1,17 +1,17 @@
 import requests
 from pdfminer.high_level import extract_text
 
+class OnlinePlagiatChecker:
 
-class Onlineplagiat :
-    
-    def onlineText(self,text):
-        url = "https://plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com/plagiarism"
-        headers = {
+    def __init__(self):
+        self.url = "https://plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com/plagiarism"
+        self.headers = {
             "Content-Type": "application/json",
-            "X-RapidAPI-Key": "0fcccf228bmshab8f84877ac8a36p14fb9ejsn5befbc823f88",
+            "X-RapidAPI-Key": "d4b76f4d56msh1c10f87d1a4df79p1b6ca1jsnc152fa391328",
             "X-RapidAPI-Host": "plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com"
         }
-        
+
+    def check_plagiarism(self, text):
         data = {
             "text": text,
             "language": "en",
@@ -19,13 +19,35 @@ class Onlineplagiat :
             "scrapeSources": False
         }
         try:
-            print(text)
-            response = requests.post(url, headers=headers, json=data)
-            response.raise_for_status()  
+            response = requests.post(self.url, headers=self.headers, json=data)
+            response.raise_for_status()
             result = response.json()
             return result
         except requests.exceptions.RequestException as e:
+            # Gérez l'erreur selon les besoins (lancer une exception, logger, etc.)
             print(e)
+
+class PDFContentExtractor:
+
+    def extract_text_from_pdf(self, pdf_path):
+        return extract_text(pdf_path)
     
-    def extraction_contenu_pdf(self,document_a_extraire):
-        return extract_text(document_a_extraire)
+    def lire_contenu_fichier(self,chemin_du_fichier):
+        try:
+            with open(chemin_du_fichier, 'r') as fichier:
+                contenu = fichier.read()
+                return contenu
+        except FileNotFoundError:
+            return f'Le fichier {chemin_du_fichier} n\'existe pas.'
+        except Exception as e:
+            return f'Une erreur s\'est produite : {str(e)}'
+    
+# #Exemple d'utilisation
+# online_plagiarism_checker = OnlinePlagiatChecker()
+
+# # Extraction du contenu PDF
+
+# # Vérification en ligne du plagiat
+# result = online_plagiarism_checker.check_plagiarism("You don't show where you are trying to reverse this URL, but it looks like you have double-quoted it. If you're using the url tag, note that you don't need quotes around the url name:")
+
+# print(result)
--- PlagiatChecker/plagiatOnline/views.py
+++ PlagiatChecker/plagiatOnline/views.py
@@ -1,7 +1,5 @@
-import pprint
 from django.shortcuts import render
-from pdfminer.high_level import extract_text
-from django.http import HttpResponseRedirect, JsonResponse
+from api.workfile import *
 from plagiatOnline.form import DocumentsForm
 from plagiatOnline.models import Documents
 from api.api_plagiat import * 
@@ -14,44 +12,66 @@ def plagiatOnline(request):
     return render(request, "online/index.html")
 
 def upload_file(request):
+    fichier = str(request.FILES['file'])
     if request.method == 'POST':
-        doc = DocumentsForm(request.POST,request.FILES)
-        resultat = ''
-        if doc.is_valid():
-            doc.save()
-            dernier = Documents.objects.latest('id')
-            documents = Documents.objects.all()
-            for document in documents:
-                if  str(document.file) != str(dernier.file):
-                    resultat = extract_pdf_content("./media/"+str(dernier.file))
-            return JsonResponse({'status': 'success', 'result': resultat })
-            
+        if Typefile(fichier) == "txt" or Typefile(fichier) == "py" or Typefile(fichier) == "c":
+            doc = DocumentsForm(request.POST,request.FILES)
+            resultat = ''
+            if doc.is_valid():
+                doc.save()
+                dernier = Documents.objects.latest('id')
+                documents = Documents.objects.all()
+                for document in documents:
+                    if  str(document.file) != str(dernier.file):
+                        resultat = extract_file_content("./media/"+str(dernier.file))
+                return JsonResponse({'status': 'success', 'result': resultat })
+                
+            else:
+                return JsonResponse({'status': 'error', 'errors': doc.errors})
         else:
-            return JsonResponse({'status': 'error', 'errors': doc.errors})
+            if Typefile(fichier) == "pdf":
+                doc = DocumentsForm(request.POST,request.FILES)
+                resultat = ''
+                if doc.is_valid():
+                    doc.save()
+                    dernier = Documents.objects.latest('id')
+                    documents = Documents.objects.all()
+                    for document in documents:
+                        if  str(document.file) != str(dernier.file):
+                            resultat = extract_pdf_content("./media/"+str(dernier.file))
+                    return JsonResponse({'status': 'success', 'result': resultat })
+                    
+                else:
+                    return JsonResponse({'status': 'error', 'errors': doc.errors})
     else:
         doc = DocumentsForm()
 
     return JsonResponse({'status': 'error', 'message': 'Method not allowed'})
 
-
 def extract_pdf_content(pdf_file):
     try:
-        online_plagiarism_checker = Onlineplagiat()
-        result = online_plagiarism_checker.extraction_contenu_pdf(pdf_file)
+        online_plagiarism_checker = PDFContentExtractor()
+        result = online_plagiarism_checker.extract_text_from_pdf(pdf_file)
+        return {'status': 'success', 'result': result}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
+
+def extract_file_content(pdf_file):
+    try:
+        online_plagiarism_checker = PDFContentExtractor()
+        result = online_plagiarism_checker.lire_contenu_fichier(pdf_file)
         return {'status': 'success', 'result': result}
     except Exception as e:
         return {'status': 'error', 'message': str(e)}
 
 def check_plagiat(request):
-    if request.method == "POST": 
+    if request.method == "POST":
         try:
-            online_plagiarism_checker = Onlineplagiat()
-            text = request.POST.get('text', '')  # Récupérer le texte de la requête POST
-            #print(text)
-            result = online_plagiarism_checker.onlineText(text)
-            return JsonResponse({'status': 'success', 'result': result})
+            online_plagiarism_checker = OnlinePlagiatChecker()
+            text = list(request.POST.keys())[0]
+            result = online_plagiarism_checker.check_plagiarism(text)
+            
+            return JsonResponse({'status': 'success', 'result': result}, content_type='application/json')  # Assurez-vous de spécifier le bon type de contenu
         except Exception as e:
             return JsonResponse({'status': 'error', 'message': str(e)})
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
-
-
